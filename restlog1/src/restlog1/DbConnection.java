822cd73ef4536d7023cd49b327a6a06002d5d8e4
SuggalaVasavi <Suggala.vasa-scholar@hcl.com>
2017-05-02 14:24:25 +0530

    singleton format updated

--- restlog1/src/restlog1/DbConnection.java
+++ restlog1/src/restlog1/DbConnection.java
@@ -1,28 +1,51 @@
 package restlog1;
 
+
+
 import java.sql.Connection;
+
 import java.sql.DriverManager;
+
 import java.sql.SQLException;
+
  
+
 public class DbConnection {
-
- 
-public static Connection getConnection()
-{
-		Connection connection=null;	
+private static DbConnection instance;
+private static Connection connection;
+	private String url="jdbc:mysql://localhost:3306/form";
+private String username="root";
+private String password="1234";
+DbConnection() throws SQLException{
 try
-{
-String connectionURL = "jdbc:mysql://localhost:3306/form";
-Class.forName("com.mysql.jdbc.Driver").newInstance();
-connection = DriverManager.getConnection(connectionURL, "root", "1234");
 
-}
-catch (Exception e)
 {
-	System.out.println(e);
-e.printStackTrace();
+
+Class.forName("com.mysql.jdbc.Driver");
+
+this.connection = DriverManager.getConnection(url,username,password);
 }
 
+catch (ClassNotFoundException e)
+
+{
+
+	System.out.println("Databsae connection failed"+e.getMessage());
+}
+}
+
+public static  Connection getConnection(){
 return connection;
+
 }
+public static DbConnection getInstance()throws SQLException{
+	if(instance == null){
+		instance = new DbConnection();
+	
+	}else 
+			if(instance.getConnection().isClosed()){
+			instance = new DbConnection();
+		}
+		return instance;
+		}
 }

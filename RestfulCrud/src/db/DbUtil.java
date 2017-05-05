92eaff5aecbeecf94d68eadb5c4acb7cbb4c0f6b
SuggalaVasavi <Suggala.vasa-scholar@hcl.com>
2017-05-05 13:01:57 +0530

    updated

--- RestfulCrud/src/db/DbUtil.java
+++ RestfulCrud/src/db/DbUtil.java
@@ -1,83 +1,16 @@
 package db;
 
-
-
 import java.sql.Connection;
-
-import java.sql.ResultSet;
-
 import java.sql.SQLException;
 
-import java.sql.Statement;
-
-import dao.DbConnection;
-
-
-
-public class DbUtil {
-
-
-
-	public static void close(DbConnection connection) {
-
-		if (connection != null) {
-
-			try {
-
-				connection.close();
-
-			} catch (SQLException e) {
-
-				
-
-			}
-
-		}
-
-	}
-
-
-
-	public static void close(Statement ps) {
-
-		if (ps != null) {
-
-			try {
-
-				ps.close();
-
-			} catch (SQLException e) {
-
-				
-
-			}
-
-		}
-
-	}
-
-
-
-	public static void close(ResultSet resultSet) {
-
-		if (resultSet != null) {
-
-			try {
-
-				resultSet.close();
-
-			} catch (SQLException e) {
-
-				
-
-			}
-
-		}
-
-	}
-
-
-
-	
-
+public class Dbutil {
+	 public static void close(Connection connection) {
+	        if (connection != null) {
+	            try {
+	                connection.close();
+	            } catch (SQLException e) {
+	                
+	            }
+	        }
+	    }
 }
